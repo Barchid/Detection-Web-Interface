cfa1ec51dbb8c37ce806086a573b04426e1e380f
Sami Barchid <sami.barchid@outlook.com>
2020-01-22 16:56:44 +0100

    add template for all detections

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ def index():
 
 @app.route('/video-detection')
 def video_detection():
-    return render_template('index.html')
+    return render_template('video-detection/video-detection.html')
 
 @app.route('/image-detection')
 def image_detection():
-    return render_template('index.html')
+    return render_template('image-detection/image-detection.html')
 
 @app.route('/webcam-detection')
 def webcam_detection():
-    return render_template('index.html')
+    return render_template('webcam-detection/webcam-detection.html')
 
 @app.route('/lol/<name>')
 def lol(name):
