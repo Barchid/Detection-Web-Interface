692cd300b6b7c20851940a4b9ca8ff5c67df4a7b
Sami Barchid <sami.barchid@outlook.com>
2020-01-22 16:44:45 +0100

    Add links to pages

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/video-detection')
+def video_detection():
+    return render_template('index.html')
+
+@app.route('/image-detection')
+def image_detection():
+    return render_template('index.html')
+
+@app.route('/webcam-detection')
+def webcam_detection():
     return render_template('index.html')
 
 @app.route('/lol/<name>')
