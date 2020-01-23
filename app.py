46e491eb268d8f6b91cd9fa0b0c75ea76f4849c7
Sami Barchid <sami.barchid@outlook.com>
2020-01-23 23:14:07 +0100

    Go socket etc

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template, url_for, jsonify
 from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO
+import eventlet
+
+print('oh')
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 CORS(app)
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/video-detection')
 def video_detection():
     return render_template('video-detection/video-detection.html')
+
 
 @app.route('/image-detection')
 def image_detection():
     return render_template('image-detection/image-detection.html')
 
+
 @app.route('/webcam-detection')
 def webcam_detection():
     return render_template('webcam-detection/webcam-detection.html')
 
-@app.route('/lol/<name>')
-def lol(name):
-    return jsonify({'name': name})
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug = True)
