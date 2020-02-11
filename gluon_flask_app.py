04a6c6525d0d0c50f7e804904cf0f9e5957a647d
Sami Barchid <sami.barchid@outlook.com>
2020-02-11 17:37:46 +0100

    debugging-zer

--- gluon_flask_app.py
+++ gluon_flask_app.py
@@ -39,7 +39,6 @@ def base64_to_image(uri):
     """"read base64 encoded uri to image"""
     encoded_data = uri.split(',')[1]
     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-    print('PUTE')
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
 
@@ -51,9 +50,9 @@ def image_preprocessing(frame, model_id):
         return gcv.data.transforms.presets.ssd.transform_test(
             frame, short=512, max_size=700)
     elif(model_id == FASTER_RCNN_ID):
-        return gcv.data.transforms.presets.rcnn.transform_test(frame)
+        return gcv.data.transforms.presets.rcnn.transform_test(frame, short=512, max_size=700)
     else:
-        return gcv.data.transforms.presets.yolo.transform_test(frame, short=512)
+        return gcv.data.transforms.presets.yolo.transform_test(frame, short=512, max_size=700)
 
 
 def model_use(x, model_id):
@@ -66,7 +65,7 @@ def model_use(x, model_id):
         return yolo(x)
 
 
-def format_predictions(class_IDs, scores, bounding_boxes, classes, threshold=0.5):
+def format_predictions(class_IDs, scores, bounding_boxes, classes, width, height, frame, threshold=0.5):
     """Formats the predictions of the gluonCV pre-trained model to the adapted results to return the right answer"""
     results = []  # results to return
 
@@ -80,11 +79,18 @@ def format_predictions(class_IDs, scores, bounding_boxes, classes, threshold=0.5
         class_id = int(class_IDs[i][0])
         bbox = bounding_boxes[i]
         name = classes[class_id]
+
+        # Adapt to original size
+        xmin = bbox[0] * width / frame.shape[1]
+        ymin = bbox[1] * height / frame.shape[0]
+        xmax = bbox[2] * width / frame.shape[1]
+        ymax = bbox[3] * height / frame.shape[0]
+
         results.append({
-            'xmin': int(bbox[0]),
-            'ymin': int(bbox[1]),
-            'xmax': int(bbox[2]),
-            'ymax': int(bbox[3]),
+            'xmin': int(xmin),
+            'ymin': int(ymin),
+            'xmax': int(xmax),
+            'ymax': int(ymax),
             'class': name,
             'score': score.item()
         })
@@ -96,11 +102,11 @@ def predict_uri(uri, model_id=SSD_ID):
     """prediction base64 image"""
     # convert uri to image in mx.nd array
     frame = base64_to_image(uri)
+    width, height = frame.shape[1], frame.shape[0]
     frame = mx.nd.array(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)).astype('uint8')
 
     # pre-processing
     x, frame = image_preprocessing(frame, model_id)
-
     # Run frame through network
     start = time.time()
     class_IDs, scores, bounding_boxes = model_use(x, model_id)
@@ -111,7 +117,7 @@ def predict_uri(uri, model_id=SSD_ID):
     bounding_boxes = bounding_boxes.asnumpy()
 
     return {
-        'predictions': format_predictions(class_IDs[0], scores[0], bounding_boxes[0], ssd.classes),
+        'predictions': format_predictions(class_IDs[0], scores[0], bounding_boxes[0], ssd.classes, width, height, frame),
         'time': end - start
     }
 
--- webcam.py
+++ webcam.py
@@ -11,12 +11,13 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='Creates a webcam detection.')
     parser.add_argument(
         '-n', '--network', help='The name of the network to use ("ssd", "frcnn" or "yolo")', default='yolo')
+    parser.add_argument('-v', '--video', help='The video file to use to perform the detection. If none, it will use the webcam.')
     args = parser.parse_args()
 
     if args.network != 'yolo' and args.network != 'frcnn' and args.network != 'ssd':
-        return 'ssd'
+        return ('ssd', args.video)
     else:
-        return args.network
+        return (args.network, args.video)
 
 
 def get_model(network):
@@ -56,15 +57,18 @@ def pre_processing(net, name, frame):
     # Use the model to make the prediction
     rgb_nd = rgb_nd.as_in_context(mx.gpu(0))
     return rgb_nd, frame
+    
 
 
-network = get_arguments()
+network, video_file = get_arguments()
 net = get_model(network)
 
 # Launch video capture
 # Load the webcam handler
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) if video_file == None else cv2.VideoCapture(video_file)
 time.sleep(1)  # letting the camera autofocus
+fourcc = cv2.VideoWriter_fourcc(*'XVID') if video_file != None else None
+out = None
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -72,7 +76,7 @@ while(cap.isOpened()):
     # break if error in reading camera
     if not ret:
         break
-    
+
     # pre-processing
     rgb_nd, frame = pre_processing(net, network, frame)
 
@@ -82,12 +86,23 @@ while(cap.isOpened()):
     # Display the result
     img = gcv.utils.viz.cv_plot_bbox(
         frame, bounding_boxes[0], scores[0], class_IDs[0], class_names=net.classes)
-    gcv.utils.viz.cv_plot_image(img)
-    k = cv2.waitKey(1)
+    
+    if video_file != None and out == None:
+        out = cv2.VideoWriter('output.avi', fourcc,
+                                  20.0, (img.shape[1], img.shape[0]))
 
+    if out != None:
+        out.write(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    else:
+        gcv.utils.viz.cv_plot_image(img, scale=2)
+
+    k = cv2.waitKey(1)
     # ESC key to stop webcam
     if k == 27:
         break
 
 cap.release()
+if out != None:
+    out.release()
+
 cv2.destroyAllWindows()
